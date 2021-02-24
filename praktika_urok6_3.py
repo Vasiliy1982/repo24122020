18db88fed2aa546c1448415700f4eef47cb50357
Vasiliy1982 <76530346+Vasiliy1982@users.noreply.github.com>
2021-02-25 00:16:40 +0700

    Add files via upload

---
+++ praktika_urok6_3.py
@@ -0,0 +1,9 @@
+#praktika_urok6_3
+
+print(" Введите четыри положительных числа")
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+number4 = int(input())
+max_number = max(number1, number2,number3,number4)
+print(max_number)
