c9617dda813a242a574bcf679062e651981cf09e
Vasiliy1982 <76530346+Vasiliy1982@users.noreply.github.com>
2021-02-25 01:14:24 +0700

    Add files via upload

---
+++ praktika_urok6_3_ версия без Max() 2.0. .py
@@ -0,0 +1,22 @@
+#praktika_urok6_3
+
+print(" Введите четыри положительных числа")
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+number4 = int(input())
+max_number = number1
+nomer = 0
+if max_number < number2:
+    max_number = number2
+    nomer = 2
+else:
+    nomer = 1
+if max_number < number3:
+    max_number = number3
+    nomer = 3
+if max_number < number4:
+    max_number=number4
+    nomer = 4
+print("число за номером",nomer,"самое большое")
+#print(max_number)
